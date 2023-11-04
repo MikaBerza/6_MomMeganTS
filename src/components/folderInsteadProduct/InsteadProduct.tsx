a3cb5524ee4529002ac85ec569da1e301f914074
Mika <mikberza@mail.ru>
2023-11-04 09:00:56 +0300

    Типизация компонента (InsteadProduct) Component Typing (InsteadProduct)

--- src/components/folderInsteadProduct/InsteadProduct.tsx
+++ src/components/folderInsteadProduct/InsteadProduct.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
 import style from './insteadProduct.module.css';
 
-function InsteadProduct({ img }) {
+type InsteadProductProps = {
+  img: string;
+};
+
+const InsteadProduct: React.FC<InsteadProductProps> = ({ img }) => {
   return (
     <div className={style['img']}>
       <img className={style['img-item']} src={img} alt='img' />
     </div>
   );
-}
+};
 
 export default InsteadProduct;
