99f91963052c8f53cf5f04d4b306b1b49aae9bf7
Mika <mikberza@mail.ru>
2023-11-04 10:01:48 +0300

    Типизация компонента (CartPage) Component Typing (CartPage)

--- src/components/pages/CartPage.tsx
+++ src/components/pages/CartPage.tsx
@@ -1,39 +1,47 @@
 import React from 'react';
-
 import { useSelector } from 'react-redux';
-
 import emptyCartImg from '../../assets/img/emptyCart.png';
-
 import MainTitle from '../folderMainTitle/MainTitle';
 import CartCard from '../folderCartCard/CartCard';
 import OrderResult from '../folderOrderResult/OrderResult';
 import ButtonGroup from '../folderButtonGroup/ButtonGroup';
 import InsteadProduct from '../folderInsteadProduct/InsteadProduct';
 
-function CartPage() {
+type CartPageProps = {
+  id: number
+  imageUrl: string;
+  title: string;
+  types: number
+  sizes: number[];
+  price: number;
+};
+const CartPage: React.FC = () => {
   /* используем хук useSelector из библиотеки Redux 
      для получения значений (productCounter, priceCounter, cartData) из состояния,
      с помощью селектора cartOfProductsSlice */
   const { productCounter, cartData } = useSelector(
-    (state) => state.cartOfProductsSlice
+    (state: any) => state.cartOfProductsSlice
   );
 
   return productCounter > 0 ? (
     <>
       <MainTitle titleName='Корзина' />
-      {cartData.map((obj) => {
+      {cartData.map((obj: CartPageProps) => {
         return <CartCard key={obj.id} {...obj} />;
       })}
       <OrderResult />
-      <ButtonGroup />
+      <ButtonGroup styleName={''} />
     </>
   ) : (
     <>
-      <MainTitle titleName='Ваша корзина пуста...' styleName={'title-item-empty'}/>
+      <MainTitle
+        titleName='Ваша корзина пуста...'
+        styleName={'title-item-empty'}
+      />
       <InsteadProduct img={emptyCartImg} />
       <ButtonGroup styleName={'hide-element'} />
     </>
   );
-}
+};
 
 export default CartPage;
